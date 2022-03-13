b405743418e63ced75afbd41fe5913fe6c4813c1
Nicholas A Christensen <nickachristensen@gmail.com>
2022-03-12 21:52:44 -0500

    built header and added basic styling

---
+++ client/src/Header/Header.styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 5rem;
+  border-bottom: 2px solid Black;
+  `
+
+export const Logo = styled.h1`
+    font-size: 3rem;
+    text-align: left;
+    padding-left: 3rem;
+`;
+
+export const Buttons = styled.h3`
+    font-size: 1.5rem;
+    text-align: right;
+    padding-right: 3rem;
+    `;
---
+++ client/src/Header/index.js
@@ -0,0 +1,18 @@
+import { Wrapper, Logo, Buttons } from './Header.styles';
+
+const Header = () => {
+    
+    return (
+        <Wrapper>
+        <Logo>
+            AVW
+        </Logo>
+        <Buttons>
+        Portfolio
+        Contact
+        </Buttons>
+        </Wrapper>
+    );
+    }
+
+export default Header;
